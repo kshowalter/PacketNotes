699166b4cc7f36e32400b7ef0d14afb9f37f2f83
Keith Showalter <kshowalter@fsec.ucf.edu>
2016-08-31 09:02:49 -0400

    Standardized action handlers

--- client/store/reducer.js
+++ client/store/reducer.js
@@ -3,7 +3,9 @@ import _ from 'lodash';
 
 var tagMarkers = ['#','@'];
 
-var addNote = function(state,action){
+var r = {};
+
+r.addNote = function(state,action){
   var id = ++state.LastNoteId;
 
   var newNote = {
@@ -42,97 +44,51 @@ var addNote = function(state,action){
 };
 
 
-//var selectTag = function(state,action){
-//  var tags = Object.assign({},state);
-//
-//  tags[action.tag].selected = ! tags[action.tag].selected;
-//
-//  return tags;
-//};
-
-/*
-var selectTag = function(state,action){
-  var tags = [...state.tags];
-
-  var tagIndex = tags.indexOf(action.tag);
-
-  if( tagIndex+1 ){
-    _.pullAt(tags, tagIndex);
-  } else {
-    tags.push(action.tag);
-  }
-
-  var searchParams = Object.assign( {}, state, {
-    tags: tags,
-    searchWords: state.textWords.concat( tags )
-  });
-  return searchParams;
-};
-*/
-
-/*
-var updateSearchString = function(state,action){
-  var searchString = action.searchString;
-
-  var searchParams = Object.assign({},state,{
-    searchString: searchString,
-    textWords: searchString.split(' '),
-    searchWords: state.searchParams.tags.concat( searchString.split(' ') )
-  });
-  return searchParams;
-};
-*/
-
-var updateFilter = function(filter,action){
-  if( action.type === 'selectTag' ){
-    filter.tags = Object.assign({}, filter.tags);
-    filter.tags[action.tag].selected = ! filter.tags[action.tag].selected;
-  } else if( action.type === 'updateSearchString' ){
-    filter = Object.assign({}, filter, {
-      searchString: action.searchString
-    });
-  }
-
+var updateFilter = function(state,action){
   var selectedTags = [];
 
-  for(var tagName in filter.tags ){
-    if( filter.tags[tagName].selected ){
+  for(var tagName in state.filter.tags ){
+    if( state.filter.tags[tagName].selected ){
       selectedTags.push(tagName);
     }
   }
 
-  var searchWords = [].concat( selectedTags, filter.searchString.split(' ') );
+  var searchWords = [].concat( selectedTags, state.filter.searchString.split(' ') );
 
   searchWords = _.filter(searchWords, function(word){
     return word !== '';
   });
 
-  filter = Object.assign({}, filter, {
+  state = Object.assign({}, state, {
     searchWords: searchWords,
     selectedTags: selectedTags
   });
 
-  return filter;
+  return state;
 };
 
 
-var updateDisplayedNotes = function(state, action){
-  if( state.filter.searchWords.length ){
-    var displayedNotes = [];
-    state.notes.forEach(function(note,id){
-      var matches = _.intersection( note.words, state.filter.searchWords );
-      if( matches.length ){
-        displayedNotes.push(id);
-      }
-    });
-  } else {
-    displayedNotes = _.keys(state.notes);
-  }
-  //state.displayedNotes = displayedNotes;
-  return displayedNotes;
+r.selectTag = function(state,action){
+  console.log(action)
+  state.filter.tags = Object.assign({}, state.filter.tags);
+  state.filter.tags[action.tag].selected = ! state.filter.tags[action.tag].selected;
+
+  return updateFilter(state,action);
 };
 
-var toggleSearchFocus = function(state){
+r.updateSearchString = function(state,action){
+  state.filter = Object.assign({}, state.filter, {
+    searchString: action.searchString
+  });
+
+  return updateFilter(state,action);
+};
+
+
+
+
+
+r.toggleSearchFocus = function(state,action){
   if( state.focus === 'searchInput'){
     state = Object.assign({}, state, {
       focus: state.focusLast
@@ -146,7 +102,7 @@ var toggleSearchFocus = function(state){
   return state;
 };
 
-var moveFocus = function(state, action){
+r.moveFocus = function(state, action){
   var direction = action.direction;
   if( direction === 'Down' && state.focus === 'notes' ){
     var newNum = state.focusNum +1;
@@ -176,14 +132,42 @@ var moveFocus = function(state, action){
   return state;
 };
 
-var r = {};
-
 r.toggleInputMode = function(state,action){
   state.inputMode = state.inputMode === '_ search' ? '+ add' : '_ search';
 
   return state;
 };
 
+r.updateTime = function(state,action){
+  state.updateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  return state;
+};
+
+r.updateTime = function(state,action){
+  state.updateTime = moment().format('YYYY-MM-DD HH:mm:ss');
+
+  return state;
+};
+
+
+
+
+var updateDisplayedNotes = function(state, action){
+  if( state.filter.searchWords.length ){
+    var displayedNotes = [];
+    state.notes.forEach(function(note,id){
+      var matches = _.intersection( note.words, state.filter.searchWords );
+      if( matches.length ){
+        displayedNotes.push(id);
+      }
+    });
+  } else {
+    displayedNotes = _.keys(state.notes);
+  }
+  //state.displayedNotes = displayedNotes;
+  return displayedNotes;
+};
 
 function reducer( existingState={}, action ){
   if( ! action ){
@@ -192,35 +176,23 @@ function reducer( existingState={}, action ){
 
   var state = _.cloneDeep(existingState);
 
-  if( [
-    'toggleInputMode'
-  ].indexOf(action.type)+1 ){
-    console.log('Reducing', action);
-    state = r[action.type](state,action);
-  } else if( action.type === 'toggleSearchFocus'){
-    state = toggleSearchFocus(state);
+  if( action.type.slice(0,2) === '@@' ){
+    console.log('Ignoring: ', action);
+    return existingState;
 
-  } else if(action.type=== 'moveFocus' ){
-    state = moveFocus(state,action);
+  } else if( [
+    'toggleInputMode',
+    'addNote',
+    'selectTag',
+    'updateSearchString',
+    'toggleSearchFocus',
+    'moveFocus',
+    'updateTime'
+  ].indexOf(action.type)+1 ){
+    state = r[action.type](state,action);
 
   } else if(action.type=== 'enterKey' ){
     console.log('Now what?');
-
-  } else if(action.type=== 'addNote' ){
-    state = addNote(state,action);
-
-  } else if(action.type=== 'updateSearchString' || action.type=== 'selectTag' ){
-    state.filter = updateFilter(state.filter, action);
-
-  } else if(action.type=== 'updateTime' ){
-    state.updateTime = moment().format('YYYY-MM-DD HH:mm:ss');
-
-  } else if(action.type=== 'element_id' ){
-    state.focus = action.elementId;
-
-  } else if( action.type.slice(0,2) === '@@' ){
-    console.log('Ignoring: ', action);
-    return existingState;
 
   } else {
     console.log('I do not know how to: ', action);
